9ee3e89780c44c3b2ffe21c4e29e8cf291d3748a
Alexander Smirnov <smirnovalnik@yandex.ru>
2023-08-12 15:44:51 +0300

    добавлен пример

--- lectures/lect2/es_lect3_ex2.c
+++ lectures/lect2/es_lect3_ex2.c
@@ -1,15 +1,48 @@
 /* Программа scat */
 
-/* Для программ на ПК */
+#include <string.h>
 #include <stdio.h>
+
 int main(int argc, char* argv[])
 {
-    /* argc - количество аргументов */
-    /* argv[0] - имя исполняемого файла,
-       argv[1] - аргумент */
-    int i;
-    for (i = 0; i < argc; i++)
-        printf("argv[%d]: %s\n", i, argv[i]);
+    // Проверка наличия параметра -n
+    int n = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-n") == 0)
+        {
+            n = 1;
+        }
+    }
+
+    // Вывод содержимого файлов
+    FILE *fp;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-n") == 0)
+            continue;
+        if (argv[i][0] == '-')
+        {
+            printf("scat: %s: Invalid option\n", argv[i]);
+            break;
+        }
+        if ((fp = fopen(argv[i], "r")) == NULL)
+        {
+            printf("scat: %s: No such file or directory\n", argv[i]);
+            continue;
+        }
+        char buf[100];
+        int j = 1;
+        while (fgets(buf, sizeof(buf), fp))
+        {
+            if (n)
+                printf("%d %s", j, buf);
+            else
+                printf("%s", buf);
+            j++;
+        }
+        fclose(fp);
+    }
 
     return 0;
 }
